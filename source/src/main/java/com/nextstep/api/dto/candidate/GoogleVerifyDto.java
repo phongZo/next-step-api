4d22408debbe9113c2a9d94a6763706641be84be
Hiep <hoanghiep8401@gmail.com>
2025-05-22 13:54:02 +0700

    fix dto

--- source/src/main/java/com/nextstep/api/dto/candidate/GoogleVerifyDto.java
+++ source/src/main/java/com/nextstep/api/dto/candidate/GoogleVerifyDto.java
@@ -1,9 +1,12 @@
 package com.nextstep.api.dto.candidate;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 @Data
 public class GoogleVerifyDto {
     public CandidateDto candidate;
+    @JsonProperty("isNew")
     public boolean isNew;
 }
