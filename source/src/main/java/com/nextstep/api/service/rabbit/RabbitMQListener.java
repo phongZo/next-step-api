1348b2b73956cc7cd08250af5c6ac64bef9f502b
Hiep <hoanghiep8401@gmail.com>
2025-06-13 22:47:38 +0700

    add Listener data embedding

--- source/src/main/java/com/nextstep/api/service/rabbit/RabbitMQListener.java
+++ source/src/main/java/com/nextstep/api/service/rabbit/RabbitMQListener.java
@@ -1,9 +1,18 @@
 package com.nextstep.api.service.rabbit;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
+
+import java.util.Map;
 
 @Service
 @Slf4j
 public class RabbitMQListener {
+    @RabbitListener(queues = "${rabbitmq.queue.data-embedding}")
+    public void handleListenDataEmbedding(String json) {
+        System.out.println("handleListenDataEmbedding");
+    }
 }
